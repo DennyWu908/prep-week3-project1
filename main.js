021448fdceb687beaad7d15903ab9cf4a0c96a9b
Denny Wu <denny.wu87@gmail.com>
2016-10-25 20:29:55 -0700

    Complete project up to question 9

--- main.js
+++ main.js
@@ -13,12 +13,18 @@ ADVANCED TRACK: 11-15
 =======================================================
 */
 
-
+document.getElementById("q0").innerHTML = "JS Page Connected Properly!";
+document.getElementById("q0").classList.add("status-good");
 
 // 1. Create a function that simply returns a string that says something funny 
 //    when it is called and then displayed into the HTML page.
 
+function sillyWord() {
+	var word = "gobsmacked";
+	document.getElementById("q1").innerHTML = word;
+};
 
+sillyWord();
 
 // 2. Define a function called "divideByTwo". 
 //    It should accept one parameter within a text field called "number" and
@@ -26,47 +32,92 @@ ADVANCED TRACK: 11-15
 //    The function should divide the number by two and 
 //    then displayed into the HTML page.
 
+function divideByTwo(number) {
+	document.getElementById("q2").innerHTML = number / 2;
+};
 
+document.getElementById("pointFive").addEventListener("click", function() {
+	var daNumber = document.getElementById("intgr").value;
+	divideByTwo(daNumber);
+});
 
 // 3. Define a function called "greeting". 
 //    It should accept two parameters within input fields, which will both be first names.
 //    The function should display to the HTML page a greeting to both people.
 
+function greeting(name1, name2) {
+	document.getElementById("q3").innerHTML = "Hello, " + name1 + " and " + name2 + "!"
+};
 
+document.getElementById("sayHello").addEventListener("click", function() {
+	var aPerson = document.getElementById("firstPerson").value;
+	var otherPerson = document.getElementById("secondPerson").value;
+	greeting(aPerson, otherPerson);
+});
 
 // 4. Create a function that finds the average of 6 numbers passed in when called 
 //    and returns the value and is then displayed in the HTML page.
 
+function average(num1, num2, num3, num4, num5, num6) {
+	return (num1 + num2 + num3 + num4 + num5 + num6) / 6
+};
 
+var averageNumber = average(1, 2, 3, 4, 5, 8);
 
+document.getElementById("q4").innerHTML = averageNumber;
 
 // 5. You use Amazon Subscribe & Save to have six cans of cashews automatically sent to you each month.
 //    Write a function that takes the price per unit and calculates the total for you each month.
 //    Since the price of the cashews can change anytime, pass that amount into the function to get your total price. 
 
+function bill(price) {
+	return price * 6
+};
 
+var totalPrice = bill(5);
 
+document.getElementById("q5").innerHTML = totalPrice + " dollars.";
 
 // 6. Define functions called "area" and "perimeter". They should both accept two parameters and calculate the area and perimeter of a rectangle.
 
+function area(height, length) {
+	return height * length
+};
 
+function parameter(height, length) {
+	return (height * 2) + (length * 2)
+}
 
+var retArea = area(5, 8);
 
+var retParam = parameter(2, 3);
+
+document.getElementById("q6").innerHTML = retArea + " and " + retParam + ".";
 
 // 7. Define a function called "quarter". It accepts one parameter called "number".
 //    The function will return a value which is one quarter of the number provided.
 
+function quarter(number) {
+	return number / 4
+};
 
+var pointTwoFive = quarter(20);
 
-
+document.getElementById("q7").innerHTML = pointTwoFive;
 
 // 8. Write a function called "sleepings", it should accept one parameter, an integer called "hours".
 //    Write a conditional statement inside of the function. If the number of hours is more than 8, display a statement to the HTML page about getting enough rest.
 //    If the number of hours is less than 8, print a statement recommending the user get more shut eye.
 
+function sleepings(hours) {
+	if (hours > 8) {
+		document.getElementById("q8").innerHTML = "You've gotten a good night's sleep!";
+	} else {
+		document.getElementById("q8").innerHTML = "You need more rest.";
+	}
+}
 
-
-
+sleepings(5);
 
 // 9. Develop a function that determines a person's age by asking them for their birth year.
 
